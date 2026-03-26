762f63792865ef3189717c36b74dd13408f60726
marianoromero8 <marianoromero97.mr@gmail.com>
2026-03-26 00:00:44 -0300

    update repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { NavBar } from "./components/NavBar";
-import AboutUs from "./sections/AboutUs";
-import { FindUs } from "./sections/FindUs";
+// import AboutUs from "./sections/AboutUs";
+// import { FindUs } from "./sections/FindUs";
 import { Home } from "./sections/Home";
 import { Catalog } from "./sections/Catalog";
-import { Reviews } from "./sections/Reviews";
+// import { Reviews } from "./sections/Reviews";
 
 function App() {
   return (
